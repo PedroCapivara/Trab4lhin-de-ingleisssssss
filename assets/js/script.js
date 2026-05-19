3fa74733a97759a0a83ce2646d40b79e81474814
PedroCapivara <152212470+PedroCapivara@users.noreply.github.com>
2026-05-19 20:53:15 -0300

    adfsghj
    asdfgchvjb

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+});
+
+hiddenElements.forEach((el) => observer.observe(el));
