e2ff824c0f380cbef23531211f5c25377ba756e8
PedroCapivara <152212470+PedroCapivara@users.noreply.github.com>
2026-05-20 22:22:07 -0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,3 +19,17 @@ const observer = new IntersectionObserver((entries) => {
 });
 
 hiddenElements.forEach((el) => observer.observe(el));
+
+const questions = document.querySelectorAll(".faq-question");
+
+questions.forEach((question) => {
+  question.addEventListener("click", () => {
+    const answer = question.nextElementSibling;
+
+    if (answer.style.maxHeight) {
+      answer.style.maxHeight = null;
+    } else {
+      answer.style.maxHeight = answer.scrollHeight + "px";
+    }
+  });
+});
